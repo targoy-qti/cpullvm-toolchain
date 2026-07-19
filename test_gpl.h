b382784e4aa0fc2771b83161f716ddd3303b6b1b
Tarun Goyal <targoy@qti.qualcomm.com>
2026-07-18 23:09:13 -0700

    Test license repo

---
+++ test_gpl.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+/*
+ * dummy_example.c
+ *
+ * Dummy GPL-2.0-only source file.
+ *
+ * Copyright (C) 2026 Your Name
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello GPL-2.0-only\n");
+    return 0;
+}
